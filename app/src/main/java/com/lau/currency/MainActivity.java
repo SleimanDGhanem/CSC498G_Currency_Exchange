195447317610c4807b4160b6e8d2607462512020
Sleiman Ghanem <sleiman.ghanem@outlook.com>
2022-02-04 00:41:08 +0200

    fixed null pointer exception and layout error

--- app/src/main/java/com/lau/currency/MainActivity.java
+++ app/src/main/java/com/lau/currency/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class MainActivity extends AppCompatActivity {
     EditText value;
 
@@ -18,17 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         value = (EditText) findViewById(R.id.value);
 
-
     }
 
     public void conversionLBP(View v){
-        float amount = Integer.parseInt(value.getText().toString());
+        double amount = Double.parseDouble(value.getText().toString());
         amount = amount * 22000;
-        value.setText(amount);
+        BigDecimal bd = new BigDecimal(amount).setScale(3, RoundingMode.HALF_UP);
+        amount = bd.doubleValue();
+        value.setText(String.valueOf(amount));
     }
     public void conversionUSD(View v){
-        float amount = Integer.parseInt(value.getText().toString());
+        double amount = Double.parseDouble(value.getText().toString());
         amount = amount / 22000;
-        value.setText(amount);
+        BigDecimal bd = new BigDecimal(amount).setScale(3, RoundingMode.HALF_UP);
+        amount = bd.doubleValue();
+        value.setText(String.valueOf(amount));
     }
 }
